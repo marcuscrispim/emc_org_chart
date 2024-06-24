f12a2dc8f9ecf5d933d52419987fd718fe88c83b
Marcus Vinícius <marcus.crispim@polo.ufsc.br>
2024-06-24 14:51:21 -0300

    criar e resolver por uma interface gráfica

---
+++ data/disciplinas.py
@@ -0,0 +1,17 @@
+disciplinas = {
+    "Cálculo I (MTM3100)": [],
+    "Física I (FSC3100)": [],
+    "Introdução à Engenharia Mecânica (EMC3100)": [],
+    "Química (QMC3100)": [],
+    "Cálculo II (MTM3101)": ["Cálculo I (MTM3100)"],
+    "Física II (FSC3101)": ["Física I (FSC3100)"],
+    "Álgebra Linear (MTM3121)": [],
+    "Geometria Analítica (MTM3111)": [],
+    "Cálculo III (MTM3102)": ["Cálculo II (MTM3101)"],
+    "Mecânica dos Sólidos A (EMC5128)": ["Álgebra Linear (MTM3121)"],
+    "Termodinâmica A (EMC5210)": ["Física I (FSC3100)", "Cálculo II (MTM3101)"], 
+    "Fenômenos de Transporte A (EMC5212)": ["Termodinâmica A (EMC5210)"],
+    "Resistência dos Materiais (EMC5131)": ["Mecânica dos Sólidos A (EMC5128)"],
+    "Processos de Fabricação A (EMC5301)": ["Introdução à Engenharia Mecânica (EMC3100)"],
+    "Análise Estrutural A (EMC5133)": ["Resistência dos Materiais (EMC5131)"]
+}
